9e55cd781f8367a4f58ccd2d93a5139e0094d0f9
Ozzy Simpson <ozzy@ozzysimpson.com>
2022-12-02 11:52:16 -0500

    Simplify d2p2 even more

--- d2-p2.py
+++ d2-p2.py
@@ -6,19 +6,18 @@ score = 0
 for line in lines:
     moves = line.replace("\n","").split(' ')
 
-    move_to_play = ''
+    move_to_play = ord(moves[0]) - ord('A')
 
     # determine move
     if moves[1] == 'X':
         # need to lose
-        move_to_play = (ord(moves[0]) - ord('A') + 2) % 3
+        move_to_play = (move_to_play + 2) % 3
     elif moves[1] == 'Y':
         # need to tie, play same move
-        move_to_play = ord(moves[0]) - ord('A')
         score += 3
     elif moves[1] == 'Z':
         # need to win
-        move_to_play = (ord(moves[0]) - ord('A') + 1) % 3
+        move_to_play = (move_to_play + 1) % 3
         score += 6
         
     # add score of move
