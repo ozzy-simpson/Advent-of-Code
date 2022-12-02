317c68756b12dde95d0291e2b490266414fb9bec
Ozzy Simpson <ozzy@ozzysimpson.com>
2022-12-02 11:00:29 -0500

    Day 1!

---
+++ day-1.py
@@ -0,0 +1,21 @@
+# open input file
+input = open('day-1-input.txt', 'r')
+lines = input.readlines()
+
+# initialize variables
+current = 0
+elves = []
+
+for line in lines:
+    # check if blank line, if so add elf to list
+    if line in ['\n', '\r\n']:
+        elves.append(current) # add elf to list
+        current = 0 # reset current amount
+        continue # skip to next line
+
+    # add current line to current amount
+    current += int(line)
+    
+# print results
+elves.sort(reverse=True)
+print("Top 3 elves are carrying", sum(elves[0:3]),"total calories")
