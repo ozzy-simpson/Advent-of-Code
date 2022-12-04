4d9abe4a63cc34f177b74585f24039046c97bce2
Ozzy Simpson <ozzy@ozzysimpson.com>
2022-12-04 11:53:44 -0500

    simplify day 4 a bit

--- d4-p2.py
+++ d4-p2.py
@@ -12,12 +12,8 @@ for line in lines:
     assignment = [eval(i) for i in assignment] # convert to int
     
     # create a list of the ranges
-    part1 = list(range(assignment[0], assignment[1]+1))
-    part2 = list(range(assignment[2], assignment[3]+1))
-
-    # convert to set
-    part1 = set(part1)
-    part2 = set(part2)
+    part1 = set(range(assignment[0], assignment[1]+1))
+    part2 = set(range(assignment[2], assignment[3]+1))
 
     # find the intersection
     intersection = part1.intersection(part2)
