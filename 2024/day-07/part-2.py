bf40b6899f178d9912d5185ad7970143c3ab4703
Ozzy Simpson <14060578+ozzy-simpson@users.noreply.github.com>
2024-12-07 10:23:08 -0500

    2024, Day 7

---
+++ 2024/day-07/part-2.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+# open input file
+with open(os.path.join(sys.path[0], "input.txt"), 'r') as input_file:
+    lines = input_file.readlines()
+
+# initialize variables
+calibration = 0
+operators = [lambda a,b: a+b, lambda a,b: a*b, lambda a,b: int(f'{a}{b}')]
+
+def checkValid(result, ints, current):
+    global operators
+
+    # Base case: If we went above desired result, return false
+    if current > result:
+        return False
+    
+    # Base case: If one int left, check operations on it
+    if len(ints) == 1:
+        for op in operators:
+            if op(current, ints[0]) == result:
+                return result
+        
+        return False # Couldn't get result, invalid
+    
+    # For each possible operation, make recursive calls on the rest of the ints
+    for op in operators:
+        if checkValid(result, ints[1:], op(current, ints[0])):
+            return result
+        
+    return False # Couldn't get result, invalid
+
+for line in lines:
+    result = int(line.split(": ")[0])
+    ints = [int(n) for n in line.split(": ")[1].split(" ")]
+
+    if checkValid(result, ints[1:], ints[0]):
+        calibration += result
+
+print(calibration)
